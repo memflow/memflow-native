3cc288f1ab116f692adb1d42cf6b68b58f8addb2
Aurimas Blažulionis <0x60@pm.me>
2021-12-24 04:23:01 +0000

    Update to latest next

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ cglue_impl_group!(NativeOs, OsInstance, {});
 
 #[os_layer_bare(name = "native")]
 pub fn build_os(
-    _args: &Args,
+    _args: &OsArgs,
     _: Option<ConnectorInstanceArcBox<'static>>,
     lib: CArc<std::ffi::c_void>,
 ) -> Result<OsInstanceArcBox<'static>> {
