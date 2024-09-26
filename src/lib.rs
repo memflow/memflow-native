93e0a9643e1327e9fdcfc4edc1c9cb9faaabc478
ko1N <ko1N1337@gmail.com>
2024-09-26 12:00:56 +0200

    Allow internal types to be accessed by end users and bump to 0.2.6

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,19 @@
 #[cfg(target_os = "linux")]
-mod linux;
+pub mod linux;
 #[cfg(target_os = "linux")]
 pub use linux::LinuxOs as NativeOs;
 #[cfg(target_os = "linux")]
 pub use linux::LinuxProcess as NativeProcess;
 
 #[cfg(target_os = "macos")]
-mod macos;
+pub mod macos;
 #[cfg(target_os = "macos")]
 pub use macos::MacOs as NativeOs;
 #[cfg(target_os = "macos")]
 pub use macos::MacProcess as NativeProcess;
 
 #[cfg(target_os = "windows")]
-mod windows;
+pub mod windows;
 #[cfg(target_os = "windows")]
 pub use self::windows::WindowsKeyboard as NativeKeyboard;
 #[cfg(target_os = "windows")]
